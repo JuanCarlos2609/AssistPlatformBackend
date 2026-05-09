c3e482bf340e63d7c7bb60b17ebfcd5259d873f1
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 <juan.orta@disrupti>
2026-05-09 14:41:30 -0600

    fix: changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,29 +27,25 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // <-- NUEVAS IMPORTACIONES
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AttendanceModule } from './attendance/attendance.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    // 1. Cargamos las variables de entorno de forma global
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    // 2. Usamos forRootAsync para esperar a que las variables carguen
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        // TypeORM lee toda la cadena de conexión desde DATABASE_URL
         url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: true, // Excelente práctica mantenerlo en false para producción
-        // IMPORTANTE para Supabase: Requiere conexión segura (SSL)
+        synchronize: true,
         ssl: {
           rejectUnauthorized: false,
         },
